d780b40dce1db593ad6ef7544a232ae68a04274e
June <junimiso04@naver.com>
2024-04-20 23:22:57 +0900

    feat: fix ripple animation logic

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,12 +7,24 @@ var rippleEffect = (function(){
     
     document.addEventListener('mousedown', function(e) {
         var parent = e.target.parentElement
-        if (e.target.classList.contains(className)) {
+        if (findClassFromParent(e.target.parentNode, className) == true ? true : false) {
+            ripple.setAttribute("style", "top: " + e.target.parentNode.offsetY + "px; left: " + e.target.parentNode.offsetX + "px");
+            e.target.appendChild(ripple)
+        } else if (e.target.classList.contains(className)) {
             ripple.setAttribute("style", "top: " + e.offsetY + "px; left: " + e.offsetX + "px");
             e.target.appendChild(ripple)
-        } else if (parent.target.classList.contains(className)) {
-            ripple.setAttribute("style", "top: " + e.offsetY + "px; left: " + e.offsetX + "px");
-            parent.appendChild(ripple)
         }
     })
 })();
+
+function findClassFromParent(parent, className) {           
+    if (parent && !parent.className) {
+        return findClassFromParent(parent.parentNode, className);
+    } else if(parent && parent.className){
+        if(!parent.classList.contains(className)){
+            return findClassFromParent(parent.parentNode, className);
+        }else {
+            return true;
+        }
+    }
+}
